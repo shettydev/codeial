02afd829c5186bd13aa2c520518c30526e2bf777
prathikshetty14 <prathikshetty1411@gmail.com>
2023-09-22 22:03:16 +0530

    something

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`mongodb+srv://prathikshetty1411:takeaguess@cluster0.8gmuyqp.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true});
-
 mongoose.set('strictQuery', false);
+
+mongoose.connect(`mongodb+srv://prathikshetty1411:takeaguess@cluster0.8gmuyqp.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 
